6e4b6bb974496a44b54c80b6a2d954f6efef9602
Antoine Garcia <a.garcia.walecha@gmail.com>
2023-02-26 11:39:40 +0100

    move files to typescript

--- pages/api/predictions/index.ts
+++ pages/api/predictions/index.ts
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
   const response = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
     headers: {
